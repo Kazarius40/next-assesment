8413023c038f8ebd36587a043a15f4429b080b49
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-13 07:46:21 +0100

    працюю над формою

--- src/app/authorization/page.tsx
+++ src/app/authorization/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 import Form from "next/form";
+import {useForm} from "react-hook-form";
+import {joiResolver} from "@hookform/resolvers/joi";
+import userAuthValidator from "@/app/validators/auth.validator";
 
 const AuthorizationPage = () => {
+    const {register, formState: {errors, isValid}} = useForm({
+        mode: 'all',
+        resolver: joiResolver(userAuthValidator),
+    });
+
 
     const loginHandler = () => {
 
