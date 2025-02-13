f0811a858adfe42e8d4ea4fda348904061affa2b
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-13 08:06:32 +0100

    зробив форму для логінації

--- src/app/authorization/page.tsx
+++ src/app/authorization/page.tsx
@@ -3,9 +3,10 @@ import Form from "next/form";
 import {useForm} from "react-hook-form";
 import {joiResolver} from "@hookform/resolvers/joi";
 import userAuthValidator from "@/app/validators/auth.validator";
+import {IAuthForm} from "@/app/models/authorization/IAuthForm";
 
 const AuthorizationPage = () => {
-    const {register, formState: {errors, isValid}} = useForm({
+    const {register, formState: {errors, isValid}} = useForm<IAuthForm>({
         mode: 'all',
         resolver: joiResolver(userAuthValidator),
     });
@@ -19,8 +20,15 @@ const AuthorizationPage = () => {
         <>
             <h1>Авторизація</h1>
             <Form action={loginHandler}>
-                <input/>
-
+                <label>
+                    <input type="text" placeholder="username" {...register("username")} />
+                    {errors.username && <div>{errors.username.message}</div>}
+                </label>
+                <label>
+                    <input type="password" placeholder="password" {...register("password")} />
+                    {errors.password && <div>{errors.password.message}</div>}
+                </label>
+                <button disabled={!isValid}>Увійти</button>
             </Form>
         </>
     );
