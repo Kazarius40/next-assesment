98a9e31cbaff81260a2144d27ef31e8b20be0246
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-18 06:16:51 +0100

    виніс форму авторизації в окремий компонент з пейджі

---
+++ src/app/components/authorization/AuthForm.tsx
@@ -0,0 +1,37 @@
+import Form from "next/form";
+import {useForm} from "react-hook-form";
+import {IAuthForm} from "@/app/models/authorization/IAuthForm";
+import {joiResolver} from "@hookform/resolvers/joi";
+import userAuthValidator from "@/app/validators/auth.validator";
+import {useRouter} from "next/navigation";
+import {loginWithToken} from "@/app/services/auth.service";
+
+export const AuthForm = () => {
+    const {register, formState: {errors, isValid}} = useForm<IAuthForm>({
+        mode: 'all',
+        resolver: joiResolver(userAuthValidator)
+    });
+    const router = useRouter();
+    const loginHandler = async (formData: FormData): Promise<void> => {
+        await loginWithToken(formData);
+        router.push("/");
+    }
+    return (
+        <>
+            <Form action={loginHandler}>
+
+                <label>
+                    <input type="text" placeholder="username" {...register("username")} />
+                    {errors.username && <div>{errors.username.message}</div>}
+                </label>
+
+                <label>
+                    <input type="password" placeholder="password" {...register("password")} />
+                    {errors.password && <div>{errors.password.message}</div>}
+                </label>
+
+                <button disabled={!isValid}>Увійти</button>
+            </Form>
+        </>
+    );
+};
--- src/app/pages/authorization/page.tsx
+++ src/app/pages/authorization/page.tsx
@@ -1,41 +1,12 @@
 "use client";
-import Form from "next/form";
-import {useForm} from "react-hook-form";
-import {joiResolver} from "@hookform/resolvers/joi";
-import userAuthValidator from "@/app/validators/auth.validator";
-import {IAuthForm} from "@/app/models/authorization/IAuthForm";
-import {useRouter} from "next/navigation";
-import {loginWithToken} from "@/app/services/auth.service";
+import {AuthForm} from "@/app/components/authorization/AuthForm";
 
 const AuthorizationPage = () => {
-    const {register, formState: {errors, isValid}} = useForm<IAuthForm>({
-        mode: 'onSubmit',
-        resolver: joiResolver(userAuthValidator)
-    });
-    const router = useRouter();
-    const loginHandler = async (formData: FormData): Promise<void> => {
-        await loginWithToken(formData);
-        router.push("/");
-    }
-
     return (
-        <>
+        <div>
             <h1>Авторизація</h1>
-            <Form action={loginHandler}>
-
-                <label>
-                    <input type="text" placeholder="username" {...register("username")} />
-                    {errors.username && <div>{errors.username.message}</div>}
-                </label>
-
-                <label>
-                    <input type="password" placeholder="password" {...register("password")} />
-                    {errors.password && <div>{errors.password.message}</div>}
-                </label>
-
-                <button disabled={!isValid}>Увійти</button>
-            </Form>
-        </>
+            <AuthForm />
+        </div>
     );
 };
 
