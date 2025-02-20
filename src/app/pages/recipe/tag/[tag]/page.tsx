2d28f51f84aa6ca8d9250f9e285fafa17ce529a5
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-02-20 04:58:52 +0100

    прибрав помилку звірки рецептів-юзерів

---
+++ src/app/pages/recipe/tag/[tag]/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+import {useEffect, useState} from "react";
+import {useParams} from "next/navigation";
+import {IRecipe} from "@/app/models/recipes/IRecipe";
+import {fetchRecipesApi} from "@/app/services/users.service";
+import {IRecipes} from "@/app/models/recipes/IRecipes";
+import Link from "next/link";
+
+export default function TagProfile() {
+    const params = useParams();
+    const tag = params?.tag as string;
+    console.log(tag);
+    const [recipes, setRecipes] = useState<IRecipe[]>([]);
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            const {total} = await fetchRecipesApi("?limit=1") as IRecipes;
+            const {recipes} = await fetchRecipesApi("?limit=" + total) as IRecipes;
+            const filteredRecipes = recipes.filter((recipe: IRecipe) => recipe.tags.includes(tag));
+            setRecipes(filteredRecipes);
+        }
+
+        fetchRecipes().catch(console.error);
+    }, [tag])
+
+    return (
+        <div>
+            <h3>Рецепти з тегом: {tag}</h3>
+            <ul>
+                {recipes.map((recipe) => (
+                    <li key={recipe.id}>
+                        <Link href={"/pages/recipe/" + recipe.id}>{recipe.name}</Link>
+                    </li>
+                ))}
+            </ul>
+
+        </div>
+    );
+};
